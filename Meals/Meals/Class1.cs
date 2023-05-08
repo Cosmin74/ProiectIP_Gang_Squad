7844b195e7ac20dd4c3801ab0bbb74bb0e19e66c
Cosmin74 <114235008+Cosmin74@users.noreply.github.com>
2023-05-08 18:04:20 +0300

    Add files via upload

---
+++ Meals/Meals/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meals
+{
+    public class Meals
+    {
+        //tipul mesei
+        public string type { get; set; }
+
+        //calorii masă
+        public double calories { get; set; }
+
+        //constructor pentru masă
+        public Meals(string mealType, double mealCalories)
+        {
+            //se asigură că au foste numere date pozitive
+            if (mealCalories < 1)
+                throw new Exception("Numărul de calorii trebuie sa fie pozitiv.");
+
+            type = mealType;
+            calories = mealCalories;
+        }
+    }
+}
